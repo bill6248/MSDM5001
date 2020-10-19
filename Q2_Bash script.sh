7c127205a9a4db5d980619a705b1f9ee6b919f34
wzw <49608785+bill6248@users.noreply.github.com>
2020-10-19 15:40:47 +0800

    Update Q2_Bash script.sh

--- Q2_Bash script.sh
+++ Q2_Bash script.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 for((i=1;i<=100;i++));do mkdir DDM$i ;done
-for((i=1;i<=100;i++));do echo -e 'nanoseconds since 1970-01-01 00:00:00 UTC:\n<'`date`'>' > DDM$i/time_till_now.txt ;done
+for((i=1;i<=100;i++));do a=`date +%s` echo -e 'nanoseconds since 1970-01-01 00:00:00 UTC:\n<'$(expr `date +%N`)'>' > DDM$i/time_till_now.txt ;done
+
+
